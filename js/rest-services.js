67ce00306f348b4b4d1c79b74e54c3327ebf1e66
osman-butt <butt.osman@gmail.com>
2023-05-12 14:53:06 +0200

    Added getMembers function

---
+++ js/rest-services.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const endpoint =
+  "https://kea-delfinen-default-rtdb.europe-west1.firebasedatabase.app/";
+
+// READ
+async function getMembers(auth) {
+  console.log("---getMembers()---");
+  const token = auth.currentUser.stsTokenManager.accessToken;
+  const response = await fetch(`${endpoint}/members.json?auth=${token}`);
+  if (response.ok) {
+    console.log("getMembers status " + response.status);
+    const data = await response.json();
+    const listOfMembers = prepareData(data);
+    return listOfMembers;
+  } else {
+    console.log(response.status, response.statusText);
+  }
+}
+
+function prepareData(dataObject) {
+  console.log("---prepareData---");
+  const flashCards = [];
+  for (const key in dataObject) {
+    const flashCard = dataObject[key];
+    flashCard.id = key;
+    flashCards.push(flashCard);
+  }
+  return flashCards;
+}
+
+export { getMembers };
--- js/view.js
+++ js/view.js
@@ -1,6 +1,8 @@
 "use strict";
 //import modules
+import { auth } from "./authentication.js";
 import { initViews, setDefaultView } from "./view-controller.js";
+import { getMembers } from "./rest-services.js";
 
 function displaySignedInUserPage() {
   const loginPage = document.querySelector("#login-page");
@@ -16,7 +18,58 @@ function displaySignedInUserPage() {
   document.querySelector("#error-response").textContent = "";
   setDefaultView();
   initViews();
+  displayMembers();
 }
+
+async function displayMembers() {
+  const table = document.querySelector("#member-table");
+  table.textContent = "";
+  const tableHeader = /*html*/ `
+   <tr>
+    <th></th>
+    <th>Navn</th>
+    <th>Email</th>
+    <th>Fødselsdato</th>
+    <th>Alder</th>
+    <th>Aktivt medlem</th>
+    <th>Aktiviteter</th>
+  </tr>`;
+  table.insertAdjacentHTML("beforeend", tableHeader);
+  const data = await getMembers(auth);
+  console.log(data);
+  data.forEach(displayMember);
+}
+
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+async function displayMember(listOfMembers) {
+  const members = document.querySelector("#member-table");
+  listOfMembers.age = getAge(listOfMembers.dob);
+  const html = /*html*/ `
+    <tr>
+      <td><img src="${
+        listOfMembers.profileImage
+      }" alt="" style="object-fit: contain; width:2em; border-radius: 2em"/></td>
+      <td>${listOfMembers.name}</td>
+      <td>${listOfMembers.email}</td>
+      <td>${listOfMembers.dob}</td>
+      <td>${listOfMembers.age}</td>
+      <td>${listOfMembers.membershipActive ? "Ja" : "Nej"}</td>
+      <td>${listOfMembers.activity.sort().join(", ")}</td>
+    </tr>
+  `;
+  members.insertAdjacentHTML("beforeend", html);
+}
+
 function displaySignedOutPage() {
   const loginPage = document.querySelector("#login-page");
   const adminPage = document.querySelector("#admin-page");
