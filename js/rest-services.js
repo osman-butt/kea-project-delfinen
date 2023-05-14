4483836797990d37bea2c35399ff8148fae44ca7
adiliqbal96 <92866917+adiliqbal96@users.noreply.github.com>
2023-05-14 14:14:36 +0200

    Create function - rest-service.js

--- js/rest-services.js
+++ js/rest-services.js
@@ -30,3 +30,34 @@ function prepareData(dataObject) {
 }
 
 export { getMembers };
+
+//CREATE
+async function createMember(name, email, dob, age, activities) {
+  console.log("---createMember()---");
+  const token = auth.currentUser.stsTokenManager.accessToken;
+
+  const newMember = {
+    name,
+    email,
+    dob,
+    age,
+    activities,
+  };
+
+  const response = await fetch(`${endpoint}/members.json?auth=${token}`, {
+    method: "POST",
+    body: JSON.stringify(newMember),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    console.log("createMember status" + response.status);
+    return await response.json();
+  } else {
+    console.log(response.status, response.statusText);
+  }
+}
+
+export { getMembers, createMember };
