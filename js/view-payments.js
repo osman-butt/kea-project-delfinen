95c38f0a25b06b98ede672b1b6ddae83c976b2f3
osman-butt <butt.osman@gmail.com>
2023-05-16 14:27:27 +0200

    payment UI & array merge

--- js/view-payments.js
+++ js/view-payments.js
@@ -1,13 +1,61 @@
 "use strict";
 
 //import modules
-import { getPayments } from "./rest-services.js";
+import { getPayments, getMembers } from "./rest-services.js";
 import { paymentsUI } from "./helpers.js";
 
 async function displayPayments() {
   paymentsUI();
-  const data = await getPayments();
-  data.forEach(displayMember);
+  const payments = await getPayments();
+  const members = await getMembers();
+  const mergedList = mergeArrays(payments, members);
+  console.log("MERGED LIST");
+  console.log(mergedList);
+  mergedList.forEach(displayPayment);
+}
+
+async function displayPayment(paymentObj) {
+  const members = document.querySelector("#payments-table");
+  const html = /*html*/ `
+  <article>
+    <tr class="payment-table-row">
+      <td><img src="${
+        paymentObj.member.profileImage
+      }" alt="" style="object-fit: contain; width:2em; border-radius: 2em"/></td>
+      <td class="col2">${paymentObj.member.name}</td>
+      <td class="col3">${Object.values(paymentObj.payments).reduce(
+        (a, b) => a + b,
+        0
+      )}</td>
+    </tr>
+  </article>
+  `;
+  members.insertAdjacentHTML("beforeend", html);
+}
+
+function mergeArrays(arr1, arr2) {
+  let res = [];
+  res = arr1.map(obj => {
+    const index = arr2.findIndex(el => el["id"] == obj["id"]);
+    const { profileImage } = index !== -1 ? arr2[index] : {};
+    const { name } = index !== -1 ? arr2[index] : {};
+    const { email } = index !== -1 ? arr2[index] : {};
+    const { membershipActive } = index !== -1 ? arr2[index] : {};
+    const { membershipDate } = index !== -1 ? arr2[index] : {};
+    const { membershipLevel } = index !== -1 ? arr2[index] : {};
+    return {
+      ...obj,
+      member: {
+        profileImage,
+        name,
+        email,
+        membershipActive,
+        membershipDate,
+        membershipLevel,
+      },
+    };
+  });
+  return res;
 }
 
 export { displayPayments };
