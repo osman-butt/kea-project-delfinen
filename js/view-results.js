9731849afe8b385ee97699c65f3bff741cc6e791
osman-butt <butt.osman@gmail.com>
2023-05-19 11:24:45 +0200

    show results ui

---
+++ js/view-results.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//import modules
+import { getPayments, getMembers } from "./rest-services.js";
+import { resultsUI } from "./helpers.js";
+
+async function displayResults() {
+  //   resultsUI();
+}
